93353c9f0864d90dc96203f252ff0211a857080e
peter4tech <peterbozz1996@gmail.com>
2026-04-02 05:44:34 +0100

    backend-patches-fix

--- backend/obj/Debug/net8.0/PortfolioAPI.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/PortfolioAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b87508331006a05a8b53a121b13aacac5ab9cee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c1893dda90a82b1b52e6d15ee35dbcf23347015")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
